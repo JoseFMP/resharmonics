e331e3998c1571f3b383c453dd48ab6013906427
Jose F. Mingorance Puga <jose@homa.co>
2021-04-20 12:27:34 +0700

    Adds new fields in property data

--- property/property.go
+++ property/property.go
@@ -1,10 +1,36 @@
 package property
 
+import "github.com/JoseFMP/resharmonics/contracts"
+
 type PropertyData struct {
-	ID         PropertyID `json:"id"`
-	Name       string     `json:"name"`
-	BuildingID BuildingID `json:"buildingId"`
+	ID                  PropertyID `json:"id"`
+	Name                string     `json:"name"`
+	BuildingID          BuildingID `json:"buildingId"`
+	BuildingName        string     `json:"buildingName"`
+	BuildingDescription string     `json:"buildingDescription"`
+
+	UnitTypeId int `json:"unitTypeId"`
+
+	Unavailable                 bool   `json:"unavailable"`
+	PropertyNameName            string `json:"propertyName"`
+	PropertyTypeDescription     string `json:"propertyTpeDescription"`
+	PropertyTypeLongDescription string `json:"propertyTypeLongDescription"`
+	ShortDescription            string `json:"shortDescription"`
+	MaxOccupancy                int    `json:"maxOccupancy"`
+	FloorSpace                  string `json:"floorSpace"`
+	Location                    string `json:"location"`
+
+	Contracts    []contracts.Contract `json:"unitContracts"`
+	CustomFields []CustomField        `json:"customFields"`
 }
 
 type PropertyID string
 type BuildingID int
+
+type CustomField struct {
+	Name  string          `json:"name"`
+	Type  CustomFieldType `json:"type"`
+	Value string          `json:"value"`
+}
+
+type CustomFieldType string
