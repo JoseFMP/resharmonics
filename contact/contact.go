622f8fefacd39d3743d6a5e3d549e53eba82db19
Jose F. Mingorance Puga <jose@homa.co>
2021-05-11 11:24:13 +0700

    Do not add fields that are empty in contact

--- contact/contact.go
+++ contact/contact.go
@@ -8,19 +8,19 @@ type Details struct {
 	FirstName               string            `json:"contactFirstName"`
 	LastName                string            `json:"contactLastName"`
 	Email                   string            `json:"email"`
-	Telephone               string            `json:"telephone"`
-	Address                 *property.Address `json:"address"`
-	PassportNumber          string            `json:"passportNumber"`
-	Nationality             string            `json:"nationality"`
-	SocialSecurity          string            `json:"socialSecurity"`
-	Referrer                string            `json:"referrer"`
-	CorporateCode           string            `json:"corporateCode"`
-	CompanyNumber           string            `json:"companyNumber"`
-	CompanyEmail            string            `json:"companyEmail"`
-	CompanyAddress          string            `json:"companyAddress"`
-	BookingContactFirstName string            `json:"bookingContactFirstName"`
-	BookingContactLastName  string            `json:"bookingContactLastName"`
-	BookingContactEmail     string            `json:"bookingContactEmail"`
+	Telephone               string            `json:"telephone,omitempty"`
+	Address                 *property.Address `json:"address,omitempty"`
+	PassportNumber          string            `json:"passportNumber,omitempty"`
+	Nationality             string            `json:"nationality,omitempty"`
+	SocialSecurity          string            `json:"socialSecurity,omitempty"`
+	Referrer                string            `json:"referrer,omitempty"`
+	CorporateCode           string            `json:"corporateCode,omitempty"`
+	CompanyNumber           string            `json:"companyNumber,omitempty"`
+	CompanyEmail            string            `json:"companyEmail,omitempty"`
+	CompanyAddress          string            `json:"companyAddress,omitempty"`
+	BookingContactFirstName string            `json:"bookingContactFirstName,omitempty"`
+	BookingContactLastName  string            `json:"bookingContactLastName,omitempty"`
+	BookingContactEmail     string            `json:"bookingContactEmail,omitempty"`
 }
 
 type ContactType string
